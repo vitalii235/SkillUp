06d06effd84f663b703c649753238e3f54f77cc7
vitalii235 <61116067+vitalii235@users.noreply.github.com>
2020-03-05 19:30:26 +0200

    Делаю блок с гридами

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__nav-bottons').toggleClass('active');
+		$('.header__burger,.header__nav-bottons,.header__title').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 
